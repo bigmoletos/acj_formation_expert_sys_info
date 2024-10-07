de4b9a4271ef324f08eaf2a708f4549d77650724
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-10-07 10:37:59 +0200

    renommage fichier

--- linux/gestion_password_fichiers_md.py
+++ linux/gestion_password_fichiers_md.py
@@ -4,16 +4,23 @@ from dotenv import load_dotenv
 # Charger le fichier .env
 load_dotenv()
 
-# Lire les valeurs
-login = os.getenv('NOM_UTILISATEUR')
-password = os.getenv('MOT_DE_PASSE')
 
-# Lire et remplacer les placeholders dans le fichier markdown
-with open('modop_install.md', 'r') as file:
-    content = file.read()
-    content = content.replace('${NOM_UTILISATEUR}', login)
-    content = content.replace('${MOT_DE_PASSE}', password)
+def inject_env_variables_to_md(input_md, output_md):
+    """Injecter les variables d'environnement dans un fichier Markdown."""
+    with open(input_md, 'r') as file:
+        content = file.read()
 
-# Écrire le nouveau contenu dans un fichier sécurisé
-with open('modop_install_securise.md', 'w') as file:
-    file.write(content)
+    # Remplacer les placeholders par les valeurs des variables d'environnement
+    for var in os.environ:
+        placeholder = f'${{{var}}}'
+        value = os.getenv(var, 'VALEUR_NON_DEFINIE')
+        content = content.replace(placeholder, value)
+
+    # Écrire le contenu modifié dans le fichier de sortie
+    with open(output_md, 'w') as file:
+        file.write(content)
+
+
+# Remplacer les placeholders dans le fichier modop_install.md
+inject_env_variables_to_md('install_wordPress_aws.md',
+                           'install_wordPress_aws_securise.md')
