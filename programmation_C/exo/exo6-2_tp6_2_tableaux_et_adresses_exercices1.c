d14f3dd2d0b16ea788e4910ce25ce0235b4cd314
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-10-14 18:15:48 +0200

    ajout controle de saisie clavier

---
+++ programmation_C/exo/exo6-2_tp6_2_tableaux_et_adresses_exercices1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+/* Enoncé
+
+
+2- Pour chaque cases, afficher l’index, le contenue et d’adresse
+3- Faire la même chose mais en utilisant la syntaxe par pointeur
+Objectifs
+tab[0] = C (0060FEF9)
+tab[1] = O (0060FEFA)
+tab[2] = D (0060FEFB)
+tab[3] = E (0060FEFC)
+tab[4] = R (0060FEFD)
+
+
+
+
+*/
+
+
+/* SPOIL
+
+1- type nom[taille] = {liste_valeurs};
+2- Il faut utiliser %p pour afficher une adresse mémoire dans un printf.
+3- tab[i] = *(tab+i)
+
+*/
+
+// Déclaration et intialisation des variables globales avec des valeurs appropriées
+
+
+int main()
+{
+    // Affiche le titre du programme
+    printf("TP 6 6-2-tableaux_et_adresses_exercice_1 \n");
+    printf("Exo1\n\n");
+
+
+
+}
