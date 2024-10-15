b6a299015035e4d1dbc26c2c0c3031918f1ceb27
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-10-15 10:14:28 +0200

    tp7 avec controle de saisie

---
+++ programmation_C/exo/exo7_tp7_4_6_table_multiplication_exercice.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+/* Enoncé
+
+1- Afficher la table de multiplication de l'entier saisie par l'utilisateur.
+2- Afficher les tables de multiplications comprise entre 0 et l’entier saisie par
+l’utilisateur.
+Objectifs
+Quelle table de multiplication afficher? 3
+3x0 = 0
+3x1 = 3
+3x2 = 6
+3x3 = 9
+
+*/
+
+
+/* SPOIL
+
+1- Utiliser une boucle for pour faire 10 multiplications (une par ligne de la table).
+2- Il faut imbriquer deux boucles. Une boucle pour les tables et une autre pour les lignes
+d’une table (point 1).
+Aide
+
+
+*/
+
+// Déclaration et intialisation des variables globales avec des valeurs appropriées
+
+
+int main()
+{
+    // Affiche le titre du programme
+    printf("\n\n====================================\n");
+    printf("tp7-4-6\n");
+    printf("Tables de multiplication \n");
+    printf("========================================\n\n");
+
+    // declaration et intialisation de nos variables
+    int nbre = 0, compteur=0, multiplicateur=10, i=0;
+    int resultat = 0;
+
+    // saisie clavier des variables
+    printf("Saisissez un nombre positif non nul: ");
+
+        // Controle de saisie
+        if ( scanf("%d", &nbre) != 1 ||nbre <=0 )
+        {
+            printf("Veuillez saisir un nombre positif non nul ! Ce n'est pas le cas de votre saisie %d\n", nbre);
+            exit(1);
+        }
+
+    // saisie clavier des variables
+        printf("Quelle table de multiplication souhaitez vous? ");
+
+        // Controle de saisie
+        if ( scanf("%d", &multiplicateur) !=1 ||multiplicateur <=0)
+        {
+            printf("Veuillez saisir un nombre positif non nul ! Ce n'est pas le cas de votre saisie %d\n",multiplicateur);
+            exit(1);
+        }
+
+    printf("\n------------------------------------\n");
+    printf("Table de %d par %d\n",nbre,multiplicateur);
+    printf("\n------------------------------------\n\n");
+
+    for (int i = 0; i < multiplicateur+1; i++)
+    {
+            resultat = nbre * i;
+            printf("%d x %d = %d\n", nbre, i, resultat);
+        for (int j = 1; j < nbre; j++)
+        {
+            // contrairement à ce qui est dit dans l'aide, je ne vois pas à quoi peut servir la 2éme boucle dans notre cas !!
+            // resultat = nbre * i;
+            // printf("%d x %d = %d\n", nbre, i, resultat);
+        }
+
+}
+
+
+
+
+    printf("\n========================================\n\n");
+
+    return 0;
+}
