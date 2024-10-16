db58326e1b864c1a5c9e113e561d8ce68c4aa27f
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-10-16 17:09:44 +0200

    supp affcihages

--- programmation_C/fonctions/fonction_tableau.c
+++ programmation_C/fonctions/fonction_tableau.c
@@ -17,34 +17,16 @@ void affichage_tableau(int *tab, size_t taille_tableau){
 
 void copie_tableau(int *tab1, int *tab2,size_t taille_tableau){
 
-    printf("\n");
-    printf("Tableau: ");
     for (size_t i = 0; i < taille_tableau; i++)
     {
-        printf("%d", *(tab1 +i));
-        if (i < taille_tableau - 1) {
-            printf(", ");
-
-        }
         *(tab2 + i) =*(tab1 + i);
-
     }
-    printf("\n\n");
 }
 
 void double_valeur_tableau(int *tab, size_t taille_tableau){
 
-    printf("\n");
-    printf("Tableau: ");
     for (size_t i = 0; i < taille_tableau; i++)
     {
-        printf("%d", *(tab +i));
-        if (i < taille_tableau - 1) {
-            printf(", ");
-
-        }
         *(tab + i) *=2;
-
     }
-    printf("\n\n");
 }
