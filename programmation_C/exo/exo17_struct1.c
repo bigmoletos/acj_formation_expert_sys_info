00d507ecb3c45ad228624fa22c0a3becc10c1851
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-10-18 11:12:15 +0200

    correction retour ligne

--- programmation_C/exo/exo17_struct1.c
+++ programmation_C/exo/exo17_struct1.c
@@ -23,19 +23,19 @@ int main(void){
     struct Personne Agent1;
     // Agent Agent1 ;
 
-    printf("veuillez saisir le nom de l'agent\n");
+    printf("veuillez saisir le nom de l'agent: ");
     scanf("%[^\n]", Agent1.name);
-    printf("veuillez saisir l'age de l'agent\n");
+    printf("veuillez saisir l'age de l'agent: ");
     scanf("%d", &Agent1.age);
 
-    printf("veuillez saisir la taille de l'agent\n");
+    printf("veuillez saisir la taille de l'agent: ");
     scanf("%f", &Agent1.taille);
 
 
 
     // Agent Agent1 = {"luis", "91", "2.26"};
 
-    printf("Carte de l'agent. \nNom: %s\nAge: %d\nTaille: %f8n",Agent1.name,Agent1.age,Agent1.taille);
+    printf("Carte de l'agent: \nNom: %s\nAge: %d\nTaille: %0.2f\n",Agent1.name,Agent1.age,Agent1.taille);
 
     return 0;
  }
