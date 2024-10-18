29dfabfdca10db7054c09ec566fc663652ea302a
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-10-18 11:52:05 +0200

    separation des fonctions entre specialées et générales

--- programmation_C/exo/exo17_struct1.c
+++ programmation_C/exo/exo17_struct1.c
@@ -10,7 +10,7 @@ Exercice Exercice 1 : Définir et afficher une structure de personne
 Énoncé : Déclare une structure Personne qui contient un nom, un âge et une taille. Crée ensuite une variable de type Personne, demande à l'utilisateur d'entrer les informations et affiche-les.
  */
 
-
+// creation de la strucute personne
 typedef struct Personne
 {
     char name[TAILLE_NAME];
@@ -27,27 +27,26 @@ void create_agent_type(Agent *a) {
 
 int main(void){
 
-    // struct Personne Agent;
+    //Creation Agent1
     Agent Agent1 ;
 
     printf("veuillez saisir le nom de l'agent: ");
     scanf("%[^\n]", Agent1.name);
+
     printf("veuillez saisir l'age de l'agent: ");
     scanf("%d", &Agent1.age);
 
     printf("veuillez saisir la taille de l'agent: ");
     scanf("%f", &Agent1.taille);
 
-
-
-
-
+//affichage Agent1
     printf("Carte de l'agent: \nNom: %s\nAge: %d\nTaille: %0.2f\n",Agent1.name,Agent1.age,Agent1.taille);
 
-    //utilization de la focntion create_agent
+    //utilization de la focntion create_agent_type
     Agent Agent2;
     create_agent_type(&Agent2);
 
+//affichage Agent2
     printf("Carte de l'agent type : \nNom: %s\nAge: %d\nTaille: %0.2f\n",Agent2.name,Agent2.age,Agent2.taille);
 
     return 0;
