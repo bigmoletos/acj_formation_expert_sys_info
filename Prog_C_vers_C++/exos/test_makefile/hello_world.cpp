95c0c1c0903e2fb023cc9b5637d40d3341347615
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-10-23 18:05:53 +0200

    correction makefile make clean-fichier

---
+++ Prog_C_vers_C++/exos/test_makefile/hello_world.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Inclure la bibliothèque d'entrée/sortie
+
+int main() {
+    // Afficher "Hello, World!" à l'écran
+    std::cout << "Hello, World!" << std::endl;
+    return 0;  // Retourner 0 pour indiquer que le programme s'est terminé avec succès
+}
