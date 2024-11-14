c77d506945e8563e6f4b102d359def6e25f6794a
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-11-14 11:57:04 +0100

    ajout tableau dynamique

--- Prog_C_vers_C++/classes/exo4_voiture_heritage_polymorphisme/src/CMoto.cpp
+++ Prog_C_vers_C++/classes/exo4_voiture_heritage_polymorphisme/src/CMoto.cpp
@@ -1,9 +1,16 @@
 #include "CMoto.hpp"
+#include <iostream>
+
+
+CMoto::CMoto(const std::string& modele) : modele(modele) {
+    // Initialisation du modèle dans le constructeur
+}
+
 
 CMoto::~CMoto() {
-    printf("Destruction de CMoto\n");
+    printf("Destruction de CMoto %s\n",modele.c_str());
 }
 
 void CMoto::afficher() const {
-    printf("Je suis une moto.\n");
+    printf("Je suis une moto.%s\n", modele.c_str());
 }
--- Prog_C_vers_C++/classes/exo4_voiture_heritage_polymorphisme/src/CVoiture.cpp
+++ Prog_C_vers_C++/classes/exo4_voiture_heritage_polymorphisme/src/CVoiture.cpp
@@ -1,9 +1,15 @@
 #include "CVoiture.hpp"
+#include <iostream>
 
+
+CVoiture::CVoiture(const std::string& modele) : modele(modele) {
+    // Initialisation du modèle dans le constructeur
+}
 CVoiture::~CVoiture() {
-    printf("Destruction de CVoiture\n");
+    printf("Destruction de CVoiture %s\n",modele.c_str());
 }
 
 void CVoiture::afficher() const {
-    printf("Je suis une voiture.\n");
+    printf("Je suis une voiture.%s\n",modele.c_str());
 }
+
