d707184a06a6e681da91077929badbd5ea1178b1
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-11-18 10:58:37 +0100

    ajout cheatsheet structure de données

--- Prog_C_vers_C++/librairies/librairie_dynamique/projet_client/exported_function.h
+++ Prog_C_vers_C++/librairies/librairie_dynamique/projet_client/exported_function.h
@@ -1,17 +1,26 @@
-#ifndef EXPORTED_FUNCTIONS_H
-#define EXPORTED_FUNCTIONS_H
-
 #ifdef _WIN32
-  #ifdef BUILD_DLL
-    #define DLL_EXPORT __declspec(dllexport)
-  #else
-    #define DLL_EXPORT __declspec(dllimport)
-  #endif
+#define DLL_EXPORT __declspec(dllexport)
 #else
-  #define DLL_EXPORT
+#define DLL_EXPORT
 #endif
 
-// Déclaration de la fonction exportée
 extern "C" DLL_EXPORT void PrintMessage();
 
-#endif // EXPORTED_FUNCTIONS_H
+//#ifndef EXPORTED_FUNCTIONS_H
+//#define EXPORTED_FUNCTIONS_H
+//
+//#ifdef _WIN32
+//  #ifdef BUILD_DLL
+//    #define DLL_EXPORT __declspec(dllexport)
+//  #else
+//    #define DLL_EXPORT __declspec(dllimport)
+//  #endif
+//#else
+//  #define DLL_EXPORT
+//#endif
+//
+//// Déclaration de la fonction exportée
+//extern "C" DLL_EXPORT void PrintMessage();
+//
+//#endif // EXPORTED_FUNCTIONS_H
+
--- Prog_C_vers_C++/librairies/librairie_dynamique/projet_client/main.cpp
+++ Prog_C_vers_C++/librairies/librairie_dynamique/projet_client/main.cpp
@@ -3,7 +3,16 @@
 #include "exported_function.h"
 
 int main() {
-    std::cout << "Appel de la fonction exportée depuis la DLL..." << std::endl;
-    PrintMessage();  // Appel de la fonction exportée
+    // PrintMessage();  // Appel de la fonction exportée
+ try {
+        PrintMessage();
+    } catch (...) {
+        std::cerr << "Failed to call PrintMessage!" << std::endl;
+    }
+    std::cout << "Appel de la fonction exportée exported_function depuis la DLL..." << std::endl;
+    // std::cout << "Execution completed." << std::endl;
+    //
+    std::cout << "Veuillez entrer une valeur : " << std::endl;
+    std::cin.get();  // Attente d'une entrée utilisateur
     return 0;
 }
