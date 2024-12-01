267090230c723bc823a6d36f832420c8660d3953
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-12-01 19:07:15 +0100

    Suppression de toutes les dépendances à la base de données

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,14 @@
 import pytest
-from app import create_app, db
+from app import create_app
 
 
 @pytest.fixture
 def app():
     app = create_app()
-    app.config.update({
-        'TESTING': True,
-        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:',
-        'WTF_CSRF_ENABLED': False
-    })
+    app.config.update({'TESTING': True, 'WTF_CSRF_ENABLED': False})
     return app
 
 
 @pytest.fixture
 def client(app):
     return app.test_client()
-
-
-@pytest.fixture
-def runner(app):
-    return app.test_cli_runner()
