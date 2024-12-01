37009aa5098c6eeec93463fb243b4e2102455e91
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-12-01 20:32:08 +0100

    ajout formulaire user

--- python_machine_learning/projets/projet_temperature/app/forms.py
+++ python_machine_learning/projets/projet_temperature/app/forms.py
@@ -8,7 +8,8 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from app.models import User
 
 
 ##
@@ -22,3 +23,21 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(),
+                                       Length(min=4, max=64)])
+    password = PasswordField('Password',
+                             validators=[DataRequired(),
+                                         Length(min=6)])
+    password2 = PasswordField('Repeat Password',
+                              validators=[DataRequired(),
+                                          EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
